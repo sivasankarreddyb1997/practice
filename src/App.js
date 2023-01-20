cf467d7c53cdff2a9494247390283fcc8473923f
sivasankarreddy0 <59655611+sivasankarreddy0@users.noreply.github.com>
2023-01-20 20:03:43 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank">
-          Learn React
-        </a>
       </header>
     </div>
   );
